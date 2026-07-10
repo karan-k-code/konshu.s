d7f318440c385fcb1b570457d7dc769731dd4a1b
Karan Kumar <nolimitedmoney4@gmail.com>
2026-07-10 17:39:00 +0530

    fix bug

---
+++ const.js
@@ -0,0 +1,51 @@
+// develaper details and soicals media links
+const developerDetails = {
+  full_name: "karan kumar",
+  email: "kitmoindia@gmail.com",
+  instagram: "https://instagram.com/karan_k_code",
+  github: "https://github.com/karan-k-code",
+  LinkedIn: "https://www.linkedin.com/in/karan-k-code",
+  x: "https://x.com/karan_k_code",
+};
+
+const kcode = {
+  url: "https://kcode.konshu.in",
+  instagram: developerDetails.instagram,
+  github: developerDetails.github,
+  LinkedIn: developerDetails.LinkedIn,
+  x: developerDetails.x,
+};
+
+const toolsCli = {
+  url: "https://tools-cli.konshu.in",
+  instagram: developerDetails.instagram,
+  github: developerDetails.github,
+  LinkedIn: developerDetails.LinkedIn,
+  x: developerDetails.x,
+};
+
+const konshu_xyz = {
+  url: "",
+  instagram: "https://instagram.com/konshu.xyz",
+  github: developerDetails.github,
+  LinkedIn: developerDetails.LinkedIn,
+  x: developerDetails.x,
+};
+
+const plant = {
+  url: "https://plant.konshu.in",
+  instagram: developerDetails.instagram,
+  github: developerDetails.github,
+  LinkedIn: developerDetails.LinkedIn,
+  x: developerDetails.x,
+};
+
+const konshu_in = {
+  instagram: "https://instagram.com/konshu.in",
+  github: developerDetails.github,
+  LinkedIn: developerDetails.LinkedIn,
+  x: developerDetails.x,
+  email: developerDetails.email,
+};
+
+// fueture
